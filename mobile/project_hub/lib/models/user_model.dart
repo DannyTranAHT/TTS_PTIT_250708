4e14814a10d51a610d8ef330487abb367476e1f2
nguyenphu191 <nguyenvanphu19012003glhd@gmail.com>
2025-07-22 19:41:16 +0700

    add UI mobile - login

---
+++ mobile/project_hub/lib/models/user_model.dart
@@ -0,0 +1,13 @@
+class UserData {
+  final String name;
+  final String email;
+  final String username;
+  final String role;
+
+  UserData({
+    required this.name,
+    required this.email,
+    required this.username,
+    required this.role,
+  });
+}
---
+++ mobile/project_hub/lib/res/images/app_images.dart
@@ -0,0 +1,20 @@
+class AppImages {
+  static const String _internalImagePath = 'lib/res/images/';
+
+  static const String clockicon = '${_internalImagePath}clock.png';
+  static const String settingicon = '${_internalImagePath}setting.png';
+  static const String profileicon = '${_internalImagePath}profile.png';
+  static const String homeicon = '${_internalImagePath}home.png';
+  static const String projecticon = '${_internalImagePath}project.png';
+  static const String profileicon2 = '${_internalImagePath}profile_select.png';
+  static const String homeicon2 = '${_internalImagePath}home_select.png';
+  static const String projecticon2 = '${_internalImagePath}project_select.png';
+  static const String avt = '${_internalImagePath}avt.png';
+  static const String starticon = '${_internalImagePath}starticon.png';
+  static const String projecthub = '${_internalImagePath}project_hub.png';
+  static const String completed = '${_internalImagePath}completed.png';
+  static const String persons = '${_internalImagePath}persons.png';
+  static const String folder = '${_internalImagePath}folder.png';
+  static const String static = '${_internalImagePath}static.png';
+  static const String note = '${_internalImagePath}note.png';
+}
